d27d12e3ff9e1014e94c8140179acb679db77b1a
MatiTorres18133 <84334646+MatiTorres18133@users.noreply.github.com>
2021-10-11 15:05:58 -0300

    main

--- JS/Form.js
+++ JS/Form.js
@@ -10,8 +10,8 @@ document.querySelector('#submit').addEventListener("click", function () {
   let servicio = document.querySelector("#servicio").value;
   let resp = document.querySelector("#respuesta");
 
-  let url = "https://api.whatsapp.com/send?phone=542995304542&text=*Start.Main*%0A*Reservas*%0A%0A*¿Cuál es tu nombre?*%0A" 
-  + cliente + "%0A*Fecha estimada*%0A" + fecha + "%0A*Servicio Solicitado*%0A" + servicio;
+  let url = "https://api.whatsapp.com/send?phone=542995304542&text=*Start.Main*%0A*Reservas*%0A%0A*Nombre*%0A" 
+  + cliente + "%0A%0A*Fecha estimada*%0A" + fecha + "%0A%0A*Servicio Solicitado*%0A" + servicio;
  
 
 
